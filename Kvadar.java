196647a17788d131c5508105f5b1ce308d2d0a3f
Sara Usevska <sarausevska42@yahoo.com>
2023-12-03 16:04:54 +0100

    first commit

---
+++ Kvadar.java
@@ -0,0 +1,20 @@
+package project_23;
+
+public class Kvadar {
+		int dolzina;
+		int visina;
+		int sirina;
+		public int getVolumen() {
+			return(dolzina*visina*sirina);
+		}
+		Kvadar (){
+	    dolzina=2;
+	    visina=3;
+	    sirina=4;
+		}
+	    Kvadar(int d,int v,int s){
+	    dolzina=d;
+	    visina=v;
+	    sirina=s;
+ }
+}	    
